e14425837ec24a4822f5931c0560f4c1a5d2e839
wangaijun <hw_wangaijun@163.com>
2019-01-09 19:41:05 +0800

    m

---
+++ src/components/test.js
@@ -0,0 +1,30 @@
+class A {
+    f() {
+        try {
+            console.log('f');
+            this.newf()
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+    newf() {
+        console.log('object');
+        let prj = '123'
+        let url = `http://www.baidu.com${prj}`
+        // this.$axios
+        //     .post(url, {})
+        //     .then((r) => console.log(r))
+    }
+}
+
+function my(p2, p1) {
+    try {
+        console.log('my');
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+export const a = new A()
+
+
---
+++ src/utils/util.js
@@ -0,0 +1,11 @@
+class Util{
+    add(a,b){
+        return a+b
+    }
+}
+// export default new Util()
+export const util = new Util() 
+export function say(){
+    return 'hello'
+}
+export const type = 'dog'
